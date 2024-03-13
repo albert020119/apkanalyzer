bcb623ea22b8b41e14516043fc3b03e6ddd213ef
Endre-Laszlo ALBERT <aendre@bitdefender.com>
2024-03-13 21:23:58 +0200

    added one more emulator

--- apk_analyzer/apk_analyzer/config/_emulator_config.py
+++ apk_analyzer/apk_analyzer/config/_emulator_config.py
@@ -1,3 +1,3 @@
 class EmulatorConfig:
     emulator_path: str = r"C:\Users\aendre\AppData\Local\Android\Sdk\emulator\emulator.exe"
-    images: list[str] = [r"@Pixel_5_API_33"]
+    images: list[str] = [r"@Pixel_5_API_33", r"@Pixel_5_API_31"]
